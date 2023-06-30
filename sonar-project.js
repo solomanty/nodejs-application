0251dd7b70d7f8ad8a47e6636e4dcbd0acb3caae
solomanty <135135251+solomanty@users.noreply.github.com>
2023-06-29 20:59:16 -0500

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -6,7 +6,7 @@ sonarqubeScanner({
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsLandmarkTechnologies',
-	    'sonar.login': 'b0bd61965a16dbbf4dbd757f5b12eb9aef8c87a2',
+	    'sonar.login': '77e83fff48d3bdfa913bb38a97d20c2ea00dbf91',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
